a2a484f612126aea3f95738fb302b3c6c41b29b0
cepedagm14 <cepedagm14@gmail.com>
2022-07-19 21:14:31 -0400

    add login and logout functions

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
 
 export const LoginPage = () => {
-
   const navigate = useNavigate();
 
+  const { login } = useContext(AuthContext);
+
   const onLogin = () => {
+    login("Aurigak");
     navigate("/", {
       replace: true,
     });
